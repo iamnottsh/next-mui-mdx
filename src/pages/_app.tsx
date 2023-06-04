a91160c461e2b3df8510bf19583f4b2bc5d036ca
gxz <cnmmail163@163.com>
2023-06-05 01:16:12 +0800

    配置mui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,29 @@
 import type {AppProps} from 'next/app'
+import {createTheme, CssBaseline, ThemeProvider, useMediaQuery} from "@mui/material";
+import {useMemo} from "react";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 export default function App({Component, pageProps}: AppProps) {
-    return <Component {...pageProps} />
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)')
+    const theme = useMemo(
+        () =>
+            createTheme({
+                palette: {
+                    mode: prefersDarkMode ? 'dark' : 'light',
+                },
+                typography: {
+                    fontSize: 12,
+                },
+            }),
+        [prefersDarkMode],
+    )
+    return (
+        <ThemeProvider theme={theme}>
+            <CssBaseline/>
+            <Component {...pageProps}/>
+        </ThemeProvider>
+    )
 }
