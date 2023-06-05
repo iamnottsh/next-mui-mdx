cf3e0a8a7cb9048dab41e31f9461c517e3c60be5
gxz <cnmmail163@163.com>
2023-06-05 18:19:33 +0800

    提取options，修复链接

---
+++ mdxOptions.mjs
@@ -0,0 +1,34 @@
+import remarkFrontmatter from "remark-frontmatter";
+import remarkFootnotes from "remark-footnotes";
+import remarkGfm from "remark-gfm";
+import remarkSupersub from "remark-supersub";
+import remarkToc from "remark-toc";
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
+import rehypeMermaid from "rehype-mermaidjs";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeHighlight from "rehype-highlight";
+
+/**
+ * @type {import("@mdx-js/loader/lib").CompileOptions}
+ */
+export default {
+    remarkPlugins: [
+        [remarkFrontmatter, ['yaml', 'toml']],
+        [remarkFootnotes, {inlineNotes: true}],
+        [remarkGfm, {singleTilde: false}],
+        remarkSupersub,
+        [remarkToc, {heading: '目录'}],
+        remarkMath,
+    ],
+    remarkRehypeOptions: {footnoteLabel: '脚注', footnoteBackLabel: '回到正文'},
+    rehypePlugins: [
+        rehypeKatex,
+        rehypeMermaid,
+        rehypeSlug,
+        [rehypeAutolinkHeadings, {content: {type: 'text', value: '¶'}}],
+        [rehypeHighlight, {ignoreMissing: true}],
+    ],
+    providerImportSource: "@mdx-js/react",
+}
--- next.config.mjs
+++ next.config.mjs
@@ -1,37 +1,9 @@
 import nextMDX from "@next/mdx";
-import remarkFrontmatter from "remark-frontmatter";
-import remarkFootnotes from "remark-footnotes";
-import remarkGfm from "remark-gfm";
-import remarkSupersub from "remark-supersub";
-import remarkToc from "remark-toc";
-import remarkMath from "remark-math";
-import rehypeKatex from "rehype-katex";
-import rehypeMermaid from "rehype-mermaidjs";
-import rehypeSlug from "rehype-slug";
-import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypeHighlight from "rehype-highlight";
+import mdxOptions from "./mdxOptions.mjs";
 
 const withMDX = nextMDX({
     extension: /\.mdx?$/,
-    options: {
-        remarkPlugins: [
-            [remarkFrontmatter, ['yaml', 'toml']],
-            [remarkFootnotes, {inlineNotes: true}],
-            [remarkGfm, {singleTilde: false}],
-            remarkSupersub,
-            [remarkToc, {heading: '目录'}],
-            remarkMath,
-        ],
-        remarkRehypeOptions: {footnoteLabel: '脚注', footnoteBackLabel: '回到正文'},
-        rehypePlugins: [
-            rehypeKatex,
-            rehypeMermaid,
-            rehypeSlug,
-            [rehypeAutolinkHeadings, {content: {type: 'text', value: '¶'}}],
-            [rehypeHighlight, {ignoreMissing: true}],
-        ],
-        providerImportSource: "@mdx-js/react",
-    },
+    options: mdxOptions,
 })
 
 /** @type {import('next').NextConfig} */
