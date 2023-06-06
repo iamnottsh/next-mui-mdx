3293c756ffaaa6d168e98d3be882a55487ddba57
gxz <cnmmail163@163.com>
2023-06-06 22:05:02 +0800

    完美mermaid

--- src/components/MDXWrapper.tsx
+++ src/components/MDXWrapper.tsx
@@ -1,6 +1,7 @@
-import {ReactNode, useEffect} from "react";
-import {Paper} from "@mui/material";
+import {ReactNode, useEffect, useRef} from "react";
+import {Paper, useTheme} from "@mui/material";
 import "katex/dist/katex.min.css";
+import mermaid from "mermaid";
 
 export default function MDXWrapper({children}: { children?: ReactNode }) {
     useEffect(() => {
@@ -12,8 +13,25 @@ export default function MDXWrapper({children}: { children?: ReactNode }) {
         }
         document.getElementById(hash)?.scrollIntoView()
     }, [])
+    const raw = useRef<HTMLDivElement | null>()
+    const ref = useRef<HTMLElement | null>()
+    const dark = useTheme().palette.mode === 'dark'
+    useEffect(() => {
+        const rawCurrent = raw.current
+        if (!rawCurrent) return
+        const refCurrent = ref.current
+        if (!refCurrent) return
+        const newNode = rawCurrent.cloneNode(true) as HTMLDivElement
+        refCurrent.replaceChildren(newNode)
+        mermaid.initialize({startOnLoad: false, theme: dark ? 'dark' : 'default'})
+        mermaid.run({nodes: newNode.querySelectorAll('pre>code.language-mermaid')}).catch(console.error)
+    }, [dark])
     return (
-        <Paper sx={{padding: 2}} className="markdown-body">
+        <Paper sx={{padding: 2}} className="markdown-body" ref={element => {
+            if (!element) return
+            if (!raw.current) raw.current = element
+            if (!ref.current) ref.current = element.parentElement
+        }}>
             {children}
         </Paper>
     )
