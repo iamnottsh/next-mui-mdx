7be00e9c93c35915c9980329d64b665db6e85ef0
gxz <cnmmail163@163.com>
2023-06-07 13:08:17 +0800

    真·完美mermaid

--- src/components/MDXWrapper.tsx
+++ src/components/MDXWrapper.tsx
@@ -1,14 +1,41 @@
-import {ReactNode} from "react";
-import {Paper} from "@mui/material";
+import {ReactNode, useEffect, useRef, memo, useMemo} from "react";
+import {Paper, useTheme} from "@mui/material";
 import "katex/dist/katex.min.css";
 import mermaid from "mermaid";
 
-mermaid.initialize({startOnLoad: false})
 export default function MDXWrapper({children}: { children?: ReactNode }) {
+    const dark = useTheme().palette.mode === 'dark'
+    const ref = useRef<HTMLDivElement | null>()
+    const pro = useRef(Promise.resolve())
+    useEffect(() => {
+        const promise = pro.current
+        if (!promise) return
+        pro.current = promise.then(() => {
+            const element = ref.current
+            if (!element) return
+            const nodes = element.querySelectorAll<HTMLElement>('pre>code.language-mermaid')
+            nodes.forEach(node => {
+                const {dataset} = node
+                node.textContent = dataset.raw ?? ''
+                delete dataset.processed
+            })
+            mermaid.initialize({startOnLoad: false, theme: dark ? 'dark' : 'default'})
+            return mermaid.run({nodes}).catch(console.error)
+        })
+    }, [dark])
+    const darkRef = useRef(dark)
     return (
         <Paper sx={{padding: 2}} className="markdown-body" ref={element => {
             if (!element) return
-            mermaid.run({nodes: element.querySelectorAll('pre>code.language-mermaid')}).catch(console.error)
+            if (ref.current) return
+            ref.current = element
+            const nodes = element.querySelectorAll<HTMLElement>('pre>code.language-mermaid')
+            nodes.forEach(node => {
+                const {dataset} = node
+                dataset.raw = node.textContent ?? undefined
+            })
+            mermaid.initialize({startOnLoad: false, theme: darkRef.current ? 'dark' : 'default'})
+            pro.current = mermaid.run({nodes}).catch(console.error)
             const {hash} = location
             if (hash === '') return
             let id
