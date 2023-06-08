996157c39c98b10a1f602fe942c13bbb4269cb3d
gxz <cnmmail163@163.com>
2023-06-08 15:50:28 +0800

    修改语言

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import {Head, Html, Main, NextScript} from 'next/document'
 
 export default function Document() {
     return (
-        <Html lang="zh">
+        <Html lang="zh-cmn-Hans">
             <Head/>
             <body>
             <Main/>
